1d314ac8d02b65bceed8c107474c8057e576e5e3
Toanproptit <nguyentrongtoana1byt@gmail.com>
2025-08-08 16:33:29 +0700

    Lan 2

--- BTGK/indext.js
+++ BTGK/indext.js
@@ -26,14 +26,47 @@ async function success(position) {
   document.getElementById("temp").textContent = `${data.main.temp}°C`;
   document.getElementById("temp_max+min").textContent = `High: ${data.main.temp_max} Low: ${data.main.temp_min}`;
 
+  
+    const sunRise = data.sys.sunrise;
+    const sunRiseDate = new Date(sunRise*1000);
+    const sunRiseTime = sunRiseDate.toLocaleTimeString("en-US",{hour: "2-digit",minute: "2-digit",hour12:true});
+  
+    const sunSet = data.sys.sunset;
+    const sunSetDate = new Date(sunSet*1000);
+    const sunSetTime = sunSetDate.toLocaleTimeString("en-US",{hour: "2-digit",minute: "2-digit",hour12:true});
+
+    const dayLengthOfDay = sunSet-sunRise
+    const hours = Math.floor(dayLengthOfDay/3600);
+    const minute = Math.floor((dayLengthOfDay%3600)/60)
+
+  document.getElementById("lengthftheday").textContent = `${hours}h ${minute}m`;
+  document.getElementById("sunset").textContent = `${sunSetTime}`;
+  document.getElementById("sunrise").textContent = ` ${sunRiseTime}`;
+
   const iconCode = data.weather[0].icon;
   const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
+  const iconUrl1 = `https://openweathermap.org/img/wn/02d@2x.png`;
+  const iconUrl2 = `https://openweathermap.org/img/wn/02d@2x.png`;
+  const iconUrl3 = `https://openweathermap.org/img/wn/01d@2x.png`;
+  const iconUrl4 = `https://openweathermap.org/img/wn/01d@2x.png`;
+  const iconUrl5 = `https://openweathermap.org/img/wn/02d@2x.png`;
+  const iconUrl6 = `https://openweathermap.org/img/wn/04d@2x.png`;
+  const iconUrl7 = `https://openweathermap.org/img/wn/04d@2x.png`;
 
+  document.getElementById("weather-icon7").src = iconUrl7;
+  document.getElementById("weather-icon6").src = iconUrl6;
+  document.getElementById("weather-icon5").src = iconUrl5;
+  document.getElementById("weather-icon4").src = iconUrl4;
+  document.getElementById("weather-icon3").src = iconUrl3;
+  document.getElementById("weather-icon2").src = iconUrl2;
+  document.getElementById("weather-icon1").src = iconUrl1;
   document.getElementById("weather-icon").src = iconUrl;
   document.getElementById("weather-icon").alt = data.weather[0].description;
   document.getElementById("main").textContent = `${data.weather[0].main}`;
   document.getElementById("feels_main").textContent = `feels like ${data.main.feels_like}`;
+  document.getElementById("feels_main1").textContent = `${data.weather[0].main}`;
+
 }
 
 function error() {
